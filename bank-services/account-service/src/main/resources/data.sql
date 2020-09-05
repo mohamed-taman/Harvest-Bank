9b72dd4ae659bc97f0628ad913709a7065f6ee81
Mohamed Taman <mohamed.taman@gmail.com>
2020-09-05 03:50:00 +0200

    Finished Account Service database.

---
+++ bank-services/account-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS account;
+
+CREATE TABLE account (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  type ENUM('CURRENT', 'SAVING') default 'CURRENT',
+  balance DECIMAL(10, 2) DEFAULT 0.0
+);
